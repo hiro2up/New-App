743502498046b6a9ae3c17a073a4ca0ded158fa8
Fabricio Nascimento <67124283+hiro2up@users.noreply.github.com>
2021-05-30 01:51:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port='8080', ssl_context=('cert.pem', 'privkey.pem'))
+
+
+#The directory structure for this app and the use of some resources, like Python packages and use of Jinja for front-end rendering,
+#were inspired by the video tutorial “Python Website Full Tutorial - Flask, Authentication, Databases & More” (https://www.youtube.com/watch?v=dam0GPOAvVI).
